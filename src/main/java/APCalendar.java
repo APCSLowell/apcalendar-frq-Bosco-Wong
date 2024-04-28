d2fae05f317b523c6cdaafe0f9495c568a878fd5
Bosco-Wong <142552491+Bosco-Wong@users.noreply.github.com>
2024-04-28 15:06:05 -0700

    Update APCalendar.java

--- src/main/java/APCalendar.java
+++ src/main/java/APCalendar.java
@@ -17,9 +17,7 @@ public class APCalendar
     if(isLeapYear(x) == true)
       count++;
     }
-      return count;
-
-  
+    return count;
   }
   
   /** Returns the value representing the day of the week for the first day of year,
